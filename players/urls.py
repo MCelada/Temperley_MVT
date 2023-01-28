e5aa06de46b08b81839a123e5e08e6e74c006484
Matias Celada <matiasicelada@gmail.com>
2023-01-28 00:16:52 -0300

    delete y edit players. Templates.

--- players/urls.py
+++ players/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from players.views import create_player, list_players
+from players.views import create_player, list_players, update_player, PlayerDeleteView
 
 urlpatterns = [
     path('create_player/', create_player),
     path('list-players/', list_players),
+    path('update_player/<int:pk>/', update_player),
+    path('delete_player/<int:pk>/', PlayerDeleteView.as_view(), name='player_delete'),
 ]
--- players/views.py
+++ players/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic import DeleteView
 from players.models import Players
 from players.forms import PlayersForm
 
@@ -47,3 +47,46 @@ def list_players(request):
     }
     return render(request, 'players/list_players.html', context=context)
 
+def update_player(request, pk):
+    player = Players.objects.get(pk=pk)
+    if request.method == 'GET':
+        context = {
+            'form': PlayersForm(
+                initial={
+                    'first_name':player.first_name,
+                    'last_name':player.last_name,
+                    'age':player.age,
+                    'height':player.height,
+                    'position':player.position,
+
+                }
+            )
+        }
+
+        return render(request, 'players/player_update.html', context=context)
+
+    elif request.method == 'POST':
+        form = PlayersForm(request.POST)
+        if form.is_valid():
+                player.first_name = form.cleaned_data['first_name']
+                player.last_name = form.cleaned_data['last_name']
+                player.age = form.cleaned_data['age']
+                player.height = form.cleaned_data['height']
+                player.position = form.cleaned_data['position']
+                player.save()
+            
+                context = {
+                    'message': 'Jugador creado exitosamente'
+                }
+        else:
+            context = {
+                'form_errors': form.errors,
+                'form': PlayersForm()
+            }
+        return render(request, 'players/player_update.html', context=context)
+
+
+class PlayerDeleteView(DeleteView):
+    model = Players
+    template_name = 'players/player_delete.html'
+    success_url = 'players/list_players.html'
